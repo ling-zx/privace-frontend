712488bf4dc35ea59e8ab74c2b315ff2aea90d23
ling-zx <72537980+ling-zx@users.noreply.github.com>
2021-05-13 22:34:34 +0800

    Update quasar.json
    change theme color

--- quasar.conf.js
+++ quasar.conf.js
@@ -116,7 +116,7 @@ module.exports = function (/* ctx */) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#C10015',
         icons: [
           {
             src: 'icons/icon-128x128.png',

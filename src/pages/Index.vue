34299490ab089242e9adebf4bf17401a3d399da4
ling-zx <72537980+ling-zx@users.noreply.github.com>
2021-05-13 22:06:54 +0800

    update(pages-index.vue)
    change characters color.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,8 +2,8 @@
   <q-page class="flex flex-center row">
     <div class="col-12" style="height: 20vh;"></div>
     <div class="col-8" style="height: 80vh;">
-      <div class ="q-mx-md text-h4 text-bold"> <div class="q-my-md text-h2 text-bold"><span class="text-purple-9">Privacy</span><span class="text-cyan-7">Wallet</span></div>
-      Taaaaaaaaaapersonal data without leaking privacy.
+      <div class ="q-mx-md text-h4 text-yellow"> <div class="q-my-md text-h2 text-bold"><span class="text-red-9">Privacy</span><span class="text-cyan-7">Wallet</span></div>
+      在不泄露隐私的情况下交易个人数据.
       </div>
       <div class="q-ma-md text-h6">
         PrivacyWallet 是一个区块链上基于差分隐私与凸优化的拍卖系统。

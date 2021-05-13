527fec8840fc489a2f58bde79187939b188617c6
ling-zx <72537980+ling-zx@users.noreply.github.com>
2021-05-14 01:01:54 +0800

    imag

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page class="flex flex-center row">
-    <div class="col-12" style="height: 20vh;"><img :src="mySrc"></div>
+    <div class="col-12" style="height: 20vh;">
+      <div class = "Visitor-img"><img :src="mySrc"></div>
+      </div>
     <div class="col-8" style="height: 80vh;">
       <div class ="q-mx-md text-h4 text-yellow"> <div class="q-my-md text-h2 text-bold"><span class="text-red-9">Privacy</span><span class="text-cyan-7">Wallet</span></div>
       在不泄露隐私的情况下交易个人数据.

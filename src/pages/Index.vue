ca99fde134043ac6010cb08c8d65880f257f090f
ling-zx <72537980+ling-zx@users.noreply.github.com>
2021-05-14 11:57:10 +0800

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <q-page class="flex flex-center row">
     <div class="col-12" style="height: 20vh;">
-      <div class = "Visitor-img"><img :src="mySrc"></div>
-      </div>
     <div class="col-8" style="height: 80vh;">
       <div class ="q-mx-md text-h4 text-yellow"> <div class="q-my-md text-h2 text-bold"><span class="text-red-9">Privacy</span><span class="text-cyan-7">Wallet</span></div>
+      <div class = "bg"><img :src="mySrc"></div>
+      </div>
       在不泄露隐私的情况下交易个人数据.
       </div>
       <div class="q-ma-md text-h6">
@@ -29,11 +29,11 @@
 </template>
 
 <script>
+import imag from "./index.png";
 export default {
   name: 'PageIndex',
   data() {
     return {
-      mySrc: "../public/favicon-32x32.png",
       slide: 'style',
       intro: {
         howItWorks: [

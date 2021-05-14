ffcd8534d69ebc8ce1bd278e88e47f6ea6efd975
ling-zx <72537980+ling-zx@users.noreply.github.com>
2021-05-14 12:10:03 +0800

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     <div class="col-12" style="height: 20vh;">
     <div class="col-8" style="height: 80vh;">
       <div class ="q-mx-md text-h4 text-yellow"> <div class="q-my-md text-h2 text-bold"><span class="text-red-9">Privacy</span><span class="text-cyan-7">Wallet</span></div>
-      <div class = "bg"><img :src="mySrc"></div>
+      <div><img :src="mySrc"></div>
       在不泄露隐私的情况下交易个人数据.
       </div>
       <div class="q-ma-md text-h6">

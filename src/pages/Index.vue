7337cb4709606298b8d5c9319da20502ffcd3265
ling-zx <72537980+ling-zx@users.noreply.github.com>
2021-05-14 12:35:02 +0800

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,5 @@
 <template>
   <q-page class="flex flex-center row">
-    <div class="bg"><img :src="mySrc"></div>
     <div class="col-12" style="height: 20vh;">
     <div class="col-8" style="height: 80vh;">
     

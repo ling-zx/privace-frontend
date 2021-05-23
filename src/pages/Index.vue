5b72aafe6088539475f450f5c613a5987db60b94
ling-zx <72537980+ling-zx@users.noreply.github.com>
2021-05-23 15:47:54 +0800

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,11 +6,11 @@
      
     <div class="col-8" style="height: 80vh;">
    
-      <div class ="q-mx-md text-h5 text-yellow"> <div class="q-my-md text-h3 text-bold"><span class="text-red">莆田学院</span><span class="text-cyan-7">基于区块链的数据交易平台</span></div>
+      <div class ="q-mx-md text-h5 text-bold"> <div class="q-my-md text-h3 text-bold"><span class="text-red">莆田学院</span><span class="text-cyan-7">基于区块链的数据交易平台</span></div>
       <div class="bg"><img :src="mySrc"></div>
       在不泄露隐私的情况下交易个人数据.
       </div>
-      <div class="q-ma-md text-h6">
+      <div class="q-ma-md text-h5">
         该数据安全交易平台是一个区块链上基于差分隐私与凸优化的拍卖系统。
         <br />本系统能够在保证公平交易和个人隐私保护的情况下，实现链上数据交易。
       </div>
